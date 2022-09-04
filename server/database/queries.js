77f7e77899c02c241fe3d3564fea1d06ae0e4d09
Dorutu <dorubota123@gmail.com>
2022-09-04 22:40:20 +0300

    added some queries

--- server/database/queries.js
+++ server/database/queries.js
@@ -1,5 +1,25 @@
 'use-strict'
 
+const getAllEvents = {
+        name: 'get-all-events',
+        text: `SELECT *
+               FROM event_table`,
+};
+
+const getAllArtist = {
+        name: 'get-all-artist',
+        text: `SELECT *
+               FROM artist_table`,
+}
+
+const getAllLocation = {
+        name: 'get-all-location',
+        text: `SELECT *
+               FROM location_table`,
+}
+
+
+
 module.exports = {
-    
+  
 }
